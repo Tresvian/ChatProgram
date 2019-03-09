7211165e381b01db54c25fd11a6dcda195b75be0
Tresvian <44061648+Tresvian@users.noreply.github.com>
2019-03-08 19:39:15 -0600

    Added debugger basic functions with levels. First edit.

--- IRC_Server_Client/debug.cpp
+++ IRC_Server_Client/debug.cpp
@@ -1,5 +1,47 @@
+#include <string>
+#include <iostream>
+#include "debug.h"
 
-namespace DEBUG
+
+using namespace std;
+
+
+//generic
+void debug::WriteMessage(std::string message)
 {
-
+	cout << message << endl;
 }
+//generic 
+
+
+void debug::SetDebugLevel(DebugLevel x)
+{
+	m_DebugLevel = x;
+}
+
+
+void debug::WriteCriticalMessage(std::string message)
+{
+	if (m_DebugLevel == DebugLevel::CriticalLevel ||
+		m_DebugLevel == DebugLevel::ImportantLevel ||
+		m_DebugLevel == DebugLevel::InformationalLevel)
+
+		debug::WriteMessage(message);
+}
+
+
+void debug::WriteImportantMessage(std::string message)
+{
+	if (m_DebugLevel == DebugLevel::CriticalLevel ||
+		m_DebugLevel == DebugLevel::ImportantLevel)
+		debug::WriteMessage(message);
+}
+
+
+
+void debug::WriteInformationalMessage(std::string message)
+{
+	if (m_DebugLevel == DebugLevel::InformationalLevel)
+		debug::WriteMessage(message);
+}
+
--- IRC_Server_Client/debug.h
+++ IRC_Server_Client/debug.h
@@ -1,1 +1,42 @@
 #pragma once
+#include <string>
+#include <iostream>
+/*
+Sets friendly console logging into the window.
+
+Each level (in enum class DebugLevel) cooresponds to the level we want to message.
+
+NoLevel        messages nothing.
+CriticalLevel  messages only critical.
+ImportantLevel messages both Critical and Important.
+Informational  messages Critical, Important, and Informational.
+
+Use SetDebugLevel(ENUM DebugLevel) for the levels.
+*/
+
+enum class DebugLevel
+{
+	NoLevel, CriticalLevel, ImportantLevel, InformationalLevel
+};
+
+
+class debug {
+private:
+	debug() = delete;
+	~debug() = delete;
+	static DebugLevel m_DebugLevel;
+
+	// Generic
+	static void WriteMessage(std::string message);
+
+public:
+	
+	static void SetDebugLevel(DebugLevel x);
+
+	static void WriteCriticalMessage (std::string message);
+
+	static void WriteImportantMessage (std::string message);
+
+	static void WriteInformationalMessage (std::string message);	
+};
+
